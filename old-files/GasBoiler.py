a47f9c8fbec48b55673701b4f8f9512ff0163f76
Klaudiusz Soltysik <klaudiusz.s1405@gmail.com>
2026-05-14 22:30:14 +0200

    ok

---
+++ old-files/GasBoiler.py
@@ -0,0 +1,17 @@
+﻿class GasBoiler:
+    def __init__(self, max_power=50.0, min_efficiency=0.9, max_efficiency=0.98):
+        self.max_power = max_power
+        self.min_efficiency = min_efficiency
+        self.max_efficiency = max_efficiency
+
+    def get_efficiency(self, current_power):
+        if current_power <= 0:
+            return self.max_efficiency
+
+        part_load = current_power / self.max_power
+
+        part_load = min(1.0, max(0.0, part_load))
+
+        efficiency = self.min_efficiency + (1.0 - part_load) * (self.max_efficiency - self.min_efficiency)
+
+        return efficiency
